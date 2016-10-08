3cf2657ee0b72248db6a9231870b5a7d9495f462
zhangaz1 <zhangaz1@hotmail.com>
2016-10-08 14:06:42 +0800

    use nbConfig

--- src/bats/changeServerInNbConfig.js
+++ src/bats/changeServerInNbConfig.js
@@ -1,1 +1,28 @@
-console.log(process.argv);
+var argv = process.argv;
+
+var configFile = argv[2];
+var config = require(configFile);
+
+var mongoDbServer = argv[3];
+if(mongoDbServer) {
+	config.mongodb_servers.servers_URL = [
+		mongoDbServer + ':27017',
+	];
+}
+
+var rabbitMqServer = argv[4];
+if(rabbitMqServer) {
+	config.amqp_servers.servers_URL = [
+		rabbitMqServer,
+	];
+}
+
+var redisServer = argv[5];
+if(redisServer) {
+	config.redis_servers.servers_URL = [
+		redisServer,
+	];
+}
+
+var fs = require('fs');
+fs.writeFile(configFile, JSON.stringify(config));
--- src/bats/hgRevertWebConfig.cmd
+++ src/bats/hgRevertWebConfig.cmd
@@ -1,4 +1,5 @@
 d:
 cd "d:/Code/Ng"
-hg revert --no-backup --rev . Web/NetBrainNGServices/src/main/csharp/com.netbrain.ng.webservices/Web.config
+::hg revert --no-backup --rev . Web/NetBrainNGServices/src/main/csharp/com.netbrain.ng.webservices/Web.config
+hg revert --no-backup --rev . Web/NetBrainNGServices/src/main/csharp/com.netbrain.ng.webservices/NBConfig.json
 c:
