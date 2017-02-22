b3233337400c8194459dc0b22d6147754a02d6c8
zhangaz1 <zhangaz1@hotmail.com>
2017-02-22 11:20:17 +0800

    add showCurrentServer

---
+++ src/bats/showCurrentServer.js
@@ -0,0 +1,5 @@
+let nbConfig = 'd:/Code/NG/Web/NetBrainNGServices/src/main/csharp/com.netbrain.ng.webservices/NBConfig.json';
+
+var currentConfig = require(nbConfig);
+
+console.log(currentConfig);
